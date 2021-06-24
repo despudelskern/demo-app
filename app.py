b7f2188281d3b4a01e00d75bca8a101be4c541ba
yannikmika <85295473+yannikmika@users.noreply.github.com>
2021-06-24 17:39:38 +0200

    alt mit ein bisschen neu

--- app.py
+++ app.py
@@ -3,6 +3,79 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
+import networkx as nx
+
+
+
+###########
+
+def generate_network(n):
+    '''
+    Generates a random geometric graph with n nodes. Analyzes the edges and
+    nodes, so that corresponding graphs can be generated. The function returns
+    the graphs.
+    @param n: Anzahl der Knoten.
+    '''
+    G = nx.random_geometric_graph(n, 0.2)   # erzeugt einen zufälligen Graphen mit n Knoten
+
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():                  # hier werden Kanten aus dem Graph extrahiert
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(        # erzeugt den Plot der die Kanten darstellt
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    node_x = []
+    node_y = []
+    for node in G.nodes():          # hier werden Knoten aus dem Graph extrahiert
+        x, y = G.nodes[node]['pos']
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(         # erzeugt den Plot der die Knoten darstellt
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(                # bestimmt nur das aussehen der Knoten
+            showscale=True,
+            colorscale='YlGnBu',
+            reversescale=True,
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    # in diesem Teil wird bestimmt, wie viele Verbindungen jeder Knoten hat
+    # das ist in den G.adjaceny() gespreichert
+    node_adjacencies = []
+    node_text = []
+    for adjacencies in G.adjacency():
+        node_adjacencies.append(len(adjacencies[1]))
+        node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+    # die Farbe der Knoten wird auf die Anzahl an Verbindungen gesetzt
+    node_trace.marker.color = node_adjacencies
+
+    # der Text der bei Hover angezeigt wird ist der node_text
+    node_trace.text = node_text
+
+    return [edge_trace, node_trace] #ausgegeben werden die beiden Plots / Traces
+
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
