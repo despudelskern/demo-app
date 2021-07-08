f1c853855443deb3efd58d2df7adbf28e98e3601
yannikmika <85295473+yannikmika@users.noreply.github.com>
2021-07-08 17:54:24 +0200

    mal sehen obs klappt

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import dash_cytoscape as cyto
+
+from WikipediaArticle import WikipediaArticle
 import plotly.graph_objs as go
 import networkx as nx
 
@@ -93,13 +96,87 @@ fig = go.Figure(data=generate_network(64),  # hier wird die Funktion von oben be
 # fig = go.Figure(data=, layout=)
 
 
+all_articles = []
+all_searchTerms = []
+G = nx.Graph()
+title = "Barack Obam"
+seite = WikipediaArticle(search_term=title)
+#seite = title
+seite.search_and_set_page()
+all_articles.append(seite)
+all_searchTerms.append(title)
+G.add_node(seite)
+
+def buildGraph(seite, tiefe):
+    if tiefe == 0:
+        return#void?
+    seite.filter_links(3)
+    for link in seite.links_filtered:
+        curr_article = WikipediaArticle(search_term=link)
+        curr_article.search_and_set_page()
+        if curr_article.error==False:
+            G.add_edge(seite,curr_article)
+            if link not in all_searchTerms:
+                all_articles.append(curr_article)#was passiert, wenn artikel bereits vorhanden--> doppelt?
+                all_searchTerms.append(link)
+                G.add_node(curr_article)
+                buildGraph(curr_article, tiefe-1)
+    return
+buildGraph(seite, 1)
+#print(G.adj)
+#for article in all_articles:
+    #print(article.search_term)
+
+elements = []
+
+
+for node in G.nodes():
+    label = node.search_term
+    id = all_searchTerms.index(node.search_term)
+    eintrag = {'data': {'id': id, 'label': label}}
+    elements.append(eintrag)
+
+for edge in G.edges():
+    node0 = edge[0]
+    node1 = edge[1]
+    id0 = all_searchTerms.index(node0.search_term)
+    id1 = all_searchTerms.index(node1.search_term)
+    eintrag = {'data': {'source': id0, 'target': id1}}
+    elements.append(eintrag)
+
+
+#===============================================================================
+# visualisation with dash
+
+app = dash.Dash(__name__)
+
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+default_stylesheet = [
+    {
+        'selector': 'node',
+        'style': {
+            'background-color': '#BFD7B5',
+            'label': 'data(label)'
+        }
+    }
+]
+
+
 
 
 # Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title='MoK'
+
 
 
 # App Layout
@@ -135,7 +212,19 @@ app.layout = html.Div([
     dcc.Graph(
         id='example-graph',
         figure=fig
-    )
+    ),
+    
+    cyto.Cytoscape(
+        id='cytoscape-event-callbacks-2',
+        layout={'name': 'cose'},
+        elements=elements,
+        stylesheet=default_stylesheet,
+        style={'width': '100%', 'height': '450px'}
+    ),
+    html.P(id='cytoscape-tapNodeData-output'),
+    html.P(id='cytoscape-tapEdgeData-output'),
+    html.P(id='cytoscape-mouseoverNodeData-output'),
+    html.P(id='cytoscape-mouseoverEdgeData-output')
 ])
 
 #===============================================================================
@@ -194,6 +283,33 @@ def update_figure(n_klicks, topic, depth):
                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
                     )
     return fig
+
+@app.callback(Output('cytoscape-tapNodeData-output', 'children'),
+              Input('cytoscape-event-callbacks-2', 'tapNodeData'))
+def displayTapNodeData(data):
+    if data:
+        return "You recently clicked/tapped the city: " + data['label']
+
+
+@app.callback(Output('cytoscape-tapEdgeData-output', 'children'),
+              Input('cytoscape-event-callbacks-2', 'tapEdgeData'))
+def displayTapEdgeData(data):
+    if data:
+        return "You recently clicked/tapped the edge between " + data['source'].upper() + " and " + data['target'].upper()
+
+
+@app.callback(Output('cytoscape-mouseoverNodeData-output', 'children'),
+              Input('cytoscape-event-callbacks-2', 'mouseoverNodeData'))
+def displayTapNodeData(data):
+    if data:
+        return "You recently hovered over the city: " + data['label']
+
+
+@app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
+              Input('cytoscape-event-callbacks-2', 'mouseoverEdgeData'))
+def displayTapEdgeData(data):
+    if data:
+        return "You recently hovered over the edge between " + data['source'].upper() + " and " + data['target'].upper()
 
 
 
